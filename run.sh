7d22548f5f61ed381791d306ebd132e9f1e91cf0
kushtrim junuzi <kushtrimjunuzi@gmail.com>
2015-04-27 07:55:38 -0400

    new env KIBANA_ES_PRESERVE_HOST

--- run.sh
+++ run.sh
@@ -2,14 +2,12 @@
 
 set -e
 
-KIBANA_ES_URL=${KIBANA_ES_URL:-http://localhost:9200}
+KIBANA_ES_URL=${KIBANA_ES_URL:-http://elasticsearch:9200}
 KIBANA_INDEX=${KIBANA_INDEX:-kibana-int}
+KIBANA_ES_PRESERVE_HOST=${KIBANA_ES_PRESERVE_HOST:-true}
 
 sed -i "s;^elasticsearch_url:.*;elasticsearch_url: ${KIBANA_ES_URL};" "/opt/kibana-${KIBANA_VERSION}/config/kibana.yml"
 sed -i "s;^kibana_index:.*;kibana_index: ${KIBANA_INDEX};" "/opt/kibana-${KIBANA_VERSION}/config/kibana.yml"
-
-# mesos-friendly change
-unset HOST
-unset PORT
+sed -i "s;^elasticsearch_preserve_host: true;elasticsearch_preserve_host: ${KIBANA_ES_PRESERVE_HOST};" "/opt/kibana-${KIBANA_VERSION}/config/kibana.yml"
 
 exec /opt/kibana-${KIBANA_VERSION}/bin/kibana
